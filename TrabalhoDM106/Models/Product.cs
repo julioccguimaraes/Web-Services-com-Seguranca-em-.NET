330ea9e26cfffb8591feb6ace177627ea4e795ba
juliocesar757 <julioguimaraes@gmail.com>
2021-06-23 09:47:22 -0300

    Publicado no Azure e criado a classe Product

---
+++ TrabalhoDM106/Models/Product.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TrabalhoDM106.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string nome { get; set; }
+
+        public string descricao { get; set; }
+
+        [Required]
+        public string codigo { get; set; }
+
+        [Required]
+        public string modelo { get; set; }
+
+        public decimal preco { get; set; }
+
+        public string cor { get; set; }
+        
+        public float peso { get; set; }
+
+        public float altura { get; set; }
+
+        public float largura { get; set; }
+
+        public float comprimento { get; set; }
+
+        public float diametro { get; set; }
+    }
+}
