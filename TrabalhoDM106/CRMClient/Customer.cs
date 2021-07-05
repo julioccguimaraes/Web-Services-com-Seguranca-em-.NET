12915a3515cc44d11cd5e183a9c389210b1d62fa
juliocesar757 <julioguimaraes@gmail.com>
2021-07-05 17:23:54 -0300

    Adicionado CRM e WebService dos Correios

---
+++ TrabalhoDM106/CRMClient/Customer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TrabalhoDM106.CRMClient
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(12)]
+        public string Cpf { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string State { get; set; }
+
+        [Required]
+        public string Country { get; set; }
+
+        [Required]
+        public string Zip { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string Mobile { get; set; }
+    }
+}
--- TrabalhoDM106/Controllers/OrdersController.cs
+++ TrabalhoDM106/Controllers/OrdersController.cs
@@ -9,6 +9,8 @@ using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Description;
 using TrabalhoDM106.Models;
+using TrabalhoDM106.br.com.correios.ws;
+using TrabalhoDM106.CRMClient;
 
 namespace TrabalhoDM106.Controllers
 {
@@ -166,6 +168,80 @@ namespace TrabalhoDM106.Controllers
             }
 
             db.Orders.Remove(order);
+            db.SaveChanges();
+
+            return Ok(order);
+        }
+        // GET: api/Orders/Frete?id=5
+        [ResponseType(typeof(Order))]
+        [HttpGet]
+        [Route("frete")]
+        public IHttpActionResult CalculaFrete(int id)
+        {
+            Order order = db.Orders.Find(id);
+
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            if (!User.IsInRole("ADMIN") && User.Identity.Name != order.Email)
+            {
+                return Unauthorized();
+            }
+
+            if(order.Status != "novo")
+            {
+                return BadRequest("O pedido de ID " + id + " não é novo pois seu status atual é: " + order.Status);
+            }
+
+            if (order.OrderProduct.Count == 0)
+            {
+                return BadRequest("O pedido de ID " + id + " não contém produtos para cálculo do frete.");
+            }
+
+            // Obtendo o CEP do cliente
+            CRMRestClient crmClient = new CRMRestClient();
+            Customer customer = crmClient.GetCustomerByEmail(order.Email);
+
+            if (customer == null)
+            {
+                return BadRequest("Falha ao consultar o CEP do cliente para cálculo do frete.");
+            }
+
+            // calculando a cubagem, o peso total e o valor total dos produtos
+            float cubagem = 0;
+            float pesoTotal = 0;
+            decimal totalProdutos = 0;
+
+            foreach (OrderProduct p in order.OrderProduct)
+            {
+                cubagem += p.Product.altura * p.Product.comprimento * p.Product.largura * p.Quantidade;
+                pesoTotal += p.Product.peso * p.Quantidade;
+                totalProdutos += p.Product.preco * p.Quantidade;
+            }
+
+            // de posse da cubagem, obtemos as dimensões do volume calculando a raiz cúbica da cubagem.
+            decimal lados = (decimal)Math.Pow(cubagem, 1/3);
+
+            CalcPrecoPrazoWS correios = new CalcPrecoPrazoWS();
+
+            // 04014 é Sedex, 04510 é Pac
+            // Para formato 1, não precisamos colocar o diâmtro do produto, deixando o valor 0
+            cResultado resultado = correios.CalcPrecoPrazo("", "", "04014", "37500007", customer.Zip, pesoTotal.ToString(), 1, lados, lados, lados, 0, "N", totalProdutos, "N");
+
+            if (resultado.Servicos[0].Erro.Equals("0"))
+            {
+                return BadRequest("Código do erro: " + resultado.Servicos[0].Erro + " - " + resultado.Servicos[0].MsgErro);
+            }
+
+            decimal valorFrete = Convert.ToDecimal(resultado.Servicos[0].Valor);
+
+            order.Peso = pesoTotal;
+            order.Frete = valorFrete;
+            order.Total = totalProdutos + valorFrete;
+            order.DataEntrega = DateTime.Today.AddDays(Convert.ToDouble(resultado.Servicos[0].PrazoEntrega));
+
             db.SaveChanges();
 
             return Ok(order);
